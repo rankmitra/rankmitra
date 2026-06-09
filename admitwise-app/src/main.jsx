b987f5060d61c5952b90640909acdfeb25dac070
Safa Fatima <safatasneem0007@gmail.com>
2026-06-09 16:27:01 +0300

    add google analytics

--- admitwise-app/src/main.jsx
+++ admitwise-app/src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App.jsx'
 import './index.css'
+
+ReactGA.initialize('G-23DFGHPDCV')
+
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
